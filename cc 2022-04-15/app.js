b83719ff2ddd8580979cecb026ccd2bc29db5b59
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-04-15 12:19:31 +0000

    15th april challenge

---
+++ cc 2022-04-15/app.js
@@ -0,0 +1,10 @@
+/* Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. */
+
+// if bool is true return "yes" else return "No"
+
+function trueOrFalse(bool){
+    if(bool){
+        return 'Yes'
+    }
+    return 'No'
+}
