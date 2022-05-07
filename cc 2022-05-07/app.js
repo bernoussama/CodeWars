fe3c77e79115f8118fb99faeb27fee432d4b8a6a
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-05-07 18:05:07 +0000

    reverse string

---
+++ cc 2022-05-07/app.js
@@ -0,0 +1,8 @@
+/* Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow' */
+function solution(str){
+    return str.split("").reverse().join("")
+}
+console.log(solution("string"))
