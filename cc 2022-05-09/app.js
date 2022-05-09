c7b830af455666a959b3350c1540d19e4486b7c1
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-05-09 08:51:28 +0100

    light switcher

---
+++ cc 2022-05-09/app.js
@@ -0,0 +1,15 @@
+/* You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, update_light('green') should return 'yellow'. */
+function updateLight(current) {
+    if(current == 'green'){
+        return 'yellow'
+    }else if(current == 'red'){
+        return 'green'
+    }
+    else if(current == 'yellow'){
+        return 'red'
+    }
+}
