b5007821305ec43ebf7cecb96fa7529280a73f08
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-05-10 13:31:23 +0100

    space remover func

---
+++ cc 2022-05-10/app.js
@@ -0,0 +1,6 @@
+/* Simple, remove the spaces from the string, then return the resultant string. */
+
+function noSpace(x){
+    return x.split(" ").join("")
+}
+console.log(noSpace("oussama bernou"))
