b5b8d9d5bc8dd93a0c2b0aaa47535a62cc9e29e7
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-05-24 13:26:07 +0100

    boolToString

---
+++ cc 2022-05-24/app.js
@@ -0,0 +1,7 @@
+/* Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given. */
+
+function booleanToString(b){
+    return b ? 'true' : 'false'
+  }
