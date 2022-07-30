8d625230fd7a1485a9a357f89929d16a621fe2d8
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-07-30 14:55:41 +0100

    filter out geese

---
+++ filterGeese.js
@@ -0,0 +1,39 @@
+/* 
+Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in your solution:
+
+  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+For example, if this array were passed as an argument:
+
+ ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated. 
+*/
+
+// PREP
+
+// Parameters: birds :array
+// Returns: birds !containing geese
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  // return an array containing all of the strings in the input array except those that match strings in geese
+  return birds.filter((el) => !geese.includes(el));
+}
+
+// Examples:
+console.log(
+  gooseFilter([
+    "Mallard",
+    "Hook Bill",
+    "African",
+    "Crested",
+    "Pilgrim",
+    "Toulouse",
+    "Blue Swedish",
+  ])
+); // ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
