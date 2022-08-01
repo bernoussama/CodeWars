340982039f7a82ab239e8525dfcc42f8d991e143
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-08-01 14:42:04 +0100

    next perfect square

--- nextPerfectSquare.js
+++ nextPerfectSquare.js
@@ -8,4 +8,52 @@ Examples:(Input --> Output)
 
 121 --> 144
 625 --> 676
-114 --> -1 since 114 is not a perfect square */
+114 --> -1 since 114 is not a perfect square 
+*/
+
+//              /* PREP */
+
+// Parameters:
+// sq: num
+
+// Returns:
+// if sq == perfectSqrt => next PerfectSqrt
+// else -1
+
+function findNextSquare(sq = 121) {
+  // Pseudo:
+
+  // check if sq is perfect square
+  let sqrt = Math.sqrt(sq);
+  // console.log(sqrt);
+  // console.log(parseInt(sqrt));
+  if (sqrt == parseInt(sqrt)) {
+    // while sq+n is not perfect square keep iterating
+    let num = Number(sq + 1);
+    let numSqrt = Math.sqrt(num);
+    while (numSqrt !== parseInt(numSqrt)) {
+      num++;
+      numSqrt = Math.sqrt(num);
+    }
+    return num;
+  }
+  // Return the next square if sq is a perfect square, -1 otherwise
+  else return -1;
+}
+
+// Examples:
+console.log(findNextSquare(121)); // returns 144
+console.log(findNextSquare(625)); // returns 676
+console.log(findNextSquare(114)); // returns -1
+
+// favourite solution
+
+/* 
+function findNextSquare(sq) {
+  var number = Math.sqrt(sq);
+  if(Math.round(number) === number) {
+    return Math.pow(++number, 2)
+  }
+  return -1;
+} 
+*/
