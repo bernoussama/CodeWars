a9d47f2bb6e7117f979694216febf8945bd635ea
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-08-03 18:26:24 +0100

    6kyu - yourOrder please - solved

---
+++ yourOrderPlease.js
@@ -0,0 +1,32 @@
+/* Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers. */
+
+function order(words) {
+  //split words by " "
+  //look for the number
+  //use the number as the index of the word
+  //push to a new array from 1 up
+
+  arr = words.split(" ");
+
+  arr2 = arr.map((el, i) => {
+    return el.split("");
+  });
+
+  indexes = arr2.map((el) => {
+    return el.filter((element) => {
+      return Number(element);
+    });
+  });
+  indexes = indexes.toString().split(",");
+
+  let ordered = [];
+  for (let i = 1; i <= indexes.length; i++) {
+    ordered.push(arr[indexes.indexOf(String(i))]);
+  }
+  return ordered.join(" ");
+}
+console.log(order("is2 Thi1s T4est 3a"));
