88e8c7383f7969133e3766149b8d3c675d71d017
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-09-09 18:13:06 +0100

    square or not

--- squareOrNot.ts
+++ squareOrNot.ts
@@ -1,19 +1,32 @@
 // Instructions
 /* 
+Write a method, that will get an integer array as parameter and will process every number from this array.
+
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+Example
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+Notes
+The input array will always contain only positive numbers, and will never be empty or null.
 
  */
 
 // /* Prep */
 
-// Parameters:
+// Parameters: array: number[]
 
-// Returns:
+// Returns: array :number[]
 
 // Examples:
-console.log(); // =>
+console.log(squareOrSquareRoot([4,3,9,7,2,1])); // => [2,9,3,49,4,1]
 
-function foo(bar) {
-  // Pseudo:
+export function squareOrSquareRoot(array:number[]) : number[] {
+  // Pseudo
+    // for every number in array check if it has a square root
+    return array.map(el=>{
+      return Number.isInteger( Math.sqrt(el) ) ? Math.sqrt(el): el**2
+    })
 
-  return;
 }
