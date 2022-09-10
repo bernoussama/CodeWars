c1d84cdff081f1c205f2118fc811a552dfc6e904
Oussama Ber <bernou@Osama>
2022-09-10 17:27:12 +0100

    if else

---
+++ ifElse.ts
@@ -0,0 +1,28 @@
+// Instructions
+/* 
+If/else syntax debug
+While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+
+checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+
+The function receives one parameter health which will always be a whole number between -10 and 10.
+ */
+
+// /* Prep */
+
+// Parameters: health: number
+
+// Returns: boolean
+
+// Examples:
+console.log(checkAlive(5)); // => true
+console.log(checkAlive(0)); // => false
+
+export function checkAlive(health: number): boolean {
+  // Fix me, please :(
+  if (health <= 0) {
+      return false;
+  } else {
+      return true;
+  }
+}
