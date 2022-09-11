16d5690734fa7a8f73d47a2a637835c7de3fef28
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-09-11 13:34:57 +0100

    square arg

---
+++ squareArg.ts
@@ -0,0 +1,19 @@
+// Instructions
+/* 
+Now you have to write a function that takes an argument and returns the square of it.
+ */
+
+// /* Prep */
+
+// Parameters:
+
+// Returns:
+
+// Examples:
+console.log(); // =>
+
+export function square(arg: number): number {
+  // Pseudo:
+
+  return arg**2
+}
