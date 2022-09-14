145556eacfcd42841be3d467163dd7007fe40c56
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-09-14 08:00:16 +0100

    nth power

---
+++ nthPower.ts
@@ -0,0 +1,31 @@
+// Instructions
+/* 
+This kata is from check py.checkio.org
+
+You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+Let's look at a few examples:
+
+array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+ */
+
+// /* Prep */
+
+// Parameters: array: number[], n: number
+
+// Returns: number
+
+// Examples:
+console.log(index( [1, 2, 3, 4], 5)); // => -1
+console.log(index( [1, 2, 3, 4], 2)); // => 9
+
+export function index(array: number[], n: number): number {
+  // pseudo
+  // select the array[n] and square it, if it doeasn't exist it will return -1
+  return array[n]? array[n]**n : -1;
+}
+
+// favorite solution*
+
+  // return array[n]**n || -1;
