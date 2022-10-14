0d82733ca615ddbf036f3bee7598dab003cb6b93
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-10-14 08:13:27 +0100

    ordered count

--- orderedCount.ts
+++ orderedCount.ts
@@ -38,6 +38,7 @@ export function orderedCount(text: string): [string, number][] {
   });
 }
 // best solution
+
 // export function orderedCount(text: string): [string, number][] {
 //   return [...new Set(text)].map((x: string) => [x, text.split(x).length - 1])
 // }
