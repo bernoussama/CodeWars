fb059e51da3dbd66d1eaad72c9d6601758cfbeae
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-11-08 12:51:15 +0100

    rps

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,19 +9,19 @@ Examples(Input1, Input2 --> Output):
 
 */
 
-function rps(p1, p2) {
-  if (p1 == p2) {
-    return "Draw!";
-  } else if (
-    (p1 == "scissors" && p2 == "paper") ||
-    (p1 == "rock" && p2 == "scissors") ||
-    (p1 == "paper" && p2 == "rock")
-  ) {
-    return "Player 1 won!";
-  } else {
-    return "Player 2 won!";
-  }
-}
+// function rps(p1, p2) {
+//   if (p1 == p2) {
+//     return "Draw!";
+//   } else if (
+//     (p1 == "scissors" && p2 == "paper") ||
+//     (p1 == "rock" && p2 == "scissors") ||
+//     (p1 == "paper" && p2 == "rock")
+//   ) {
+//     return "Player 1 won!";
+//   } else {
+//     return "Player 2 won!";
+//   }
+// }
 
 // favorite solution (even though the above is more descriptive )
 
