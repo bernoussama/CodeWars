b0e939da9a0e53a82d337f8da1dfe8d082cd3941
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-01-14 21:26:04 +0100

    recursive persistence in js

---
+++ persistence.js
@@ -0,0 +1,30 @@
+// Instructions
+/* 
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)
+
+ */
+
+// /* Prep */
+
+// Parameters:
+
+// Returns:
+
+// Examples:
+
+
+function persistence(num, _p = 0) {
+  // Pseudo:
+  let mult = (n) => String(n).split("").map(v => Number(v)).reduce((acc, v) => acc * v)
+  let n = mult(num)
+  return n == num ? _p : persistence(n, ++_p);
+
+}
+
+console.log(persistence(39)); // =>
